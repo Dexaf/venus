e854df2491a073dc9bfa7abe5089e382d4911033
enhance <eu.project.enhance@gmail.com>
2025-07-01 18:26:26 +0200

    FEAT: changed interfaces to use primitive meshes

--- src/lib/interfaces/behaviourObject.interface.ts
+++ src/lib/interfaces/behaviourObject.interface.ts
@@ -1,8 +1,10 @@
+import { AnimationMixer } from "three";
 import { VenusRenderer } from "../renderer/venusRenderer";
 
 export interface IBehaviourObject<T> {
 	obj: T;
 	key: string;
+  animationMixer?: AnimationMixer; 
 	tag?: string;
 	OnAdd?(venusRenderer: VenusRenderer): void;
 	OnRemove?(venusRenderer: VenusRenderer): void;
--- src/lib/interfaces/terraformState.interface.ts
+++ src/lib/interfaces/terraformState.interface.ts
@@ -1,10 +1,10 @@
-import { IBehaviourLight, ILocatedBehaviourObject3D } from "./terraformObjects.interface";
+import { IBehaviourLight, ILocatedBehaviourObject3D, IBehaviourPrimitiveObject3D } from "./terraformObjects.interface";
 import * as THREE from "three";
 import { IAudioConfig } from "./audioConfig.interface";
 
 export interface ITerraformState {
 	camera: THREE.Camera;
-	objects: ILocatedBehaviourObject3D[];
+	objects: (IBehaviourPrimitiveObject3D | ILocatedBehaviourObject3D)[];
 	lights: IBehaviourLight[];
 	audios: IAudioConfig[];
 }
