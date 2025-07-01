4d8741ef368aa1be13d2f629d0e6fa1c4c514051
enhance <eu.project.enhance@gmail.com>
2025-07-01 23:45:00 +0200

    FEAT: added properties and animations to behaviourobjects

--- src/lib/interfaces/behaviourObject.interface.ts
+++ src/lib/interfaces/behaviourObject.interface.ts
@@ -1,10 +1,12 @@
-import { AnimationMixer } from "three";
+import { AnimationClip, AnimationMixer } from "three";
 import { VenusRenderer } from "../renderer/venusRenderer";
 
 export interface IBehaviourObject<T> {
 	obj: T;
+	animations?: AnimationClip[];
+	properties?: Map<string, any>;
 	key: string;
-  animationMixer?: AnimationMixer; 
+	animationMixer?: AnimationMixer;
 	tag?: string;
 	OnAdd?(venusRenderer: VenusRenderer): void;
 	OnRemove?(venusRenderer: VenusRenderer): void;
