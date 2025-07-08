599d045198c94e7746439849df8c9c7e2adebe38
enhance <eu.project.enhance@gmail.com>
2025-07-08 16:40:40 +0200

    FEAT: added walking demo

--- public/venus-demo/src/assets/example-scene/objects3D/debugSpline.ts
+++ public/venus-demo/src/assets/example-scene/objects3D/debugSpline.ts
@@ -7,14 +7,16 @@ import {
 } from 'three';
 import { IBehaviourPrimitiveObject3D } from '../../../../../../dist/lib/interfaces/terraformObjects.interface';
 
-const debugSplineKey = 'spline_0';
+export const debugSplineKey = 'spline_0';
+
+export const catmullRomCurve3Params = [
+  new Vector3(-10, 0, -40),
+  new Vector3(10, 0, 0),
+  new Vector3(-10, 0, 40),
+];
 
 const GetDebugSpline = () => {
-  const curve = new CatmullRomCurve3([
-    new Vector3(-10, 0, -40),
-    new Vector3(10, 0, 0),
-    new Vector3(-10, 0, 40),
-  ]);
+  const curve = new CatmullRomCurve3([...catmullRomCurve3Params]);
   const points = curve.getPoints(20);
   const geometry = new BufferGeometry().setFromPoints(points);
   const material = new LineBasicMaterial({ color: 0xff0000 });
