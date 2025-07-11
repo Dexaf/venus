17271e9c61b0840d895111b823f9b6318169932b
Dexaf <federicopisajob@gmail.com>
2025-07-11 22:34:05 +0200

    FEAT: created rover a framework for registering and swapping controllers

---
+++ src/lib/interfaces/roverController.interface.ts
@@ -0,0 +1,21 @@
+//NOTE -  yea any on event is orrendous, but i didn't want to use
+//        different object for every single type of event function
+export interface IRoverController {
+	inputs: {
+		[key: string]: IRoverInput;
+	};
+	disabled: boolean;
+	events: {
+		eventType: keyof DocumentEventMap;
+		event: any;
+	}[];
+	name: string;
+}
+
+export interface IRoverInput {
+	type: "keyboard" | "pointer" | "pointermove";
+	value: string;
+	alias: string;
+	isTapped: boolean;
+	event?: Event;
+}
