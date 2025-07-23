dc3a09f18a9fb218f8657581ce8101b927516386
Dexaf <federicopisajob@gmail.com>
2025-07-23 16:43:07 +0200

    changed export setting for npm package

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,5 @@
-"use strict";
+import { VenusRenderer } from "./lib/renderer/venusRenderer";
+import { SetupRenderer } from "./lib/renderer/setupRenderer";
+import { Rover } from "./lib/rover/rover";
+import { Terraform } from "./lib/terraform/terraform";
+export { VenusRenderer, SetupRenderer, Rover, Terraform };
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { VenusRenderer } from "./lib/renderer/venusRenderer";
+import { SetupRenderer } from "./lib/renderer/setupRenderer";
+import { Rover } from "./lib/rover/rover";
+import { Terraform } from "./lib/terraform/terraform";
+import { IAudioConfig } from "./lib/interfaces/audioConfig.interface";
+import { IBehaviourObject, IBehaviourObjectChildren } from "./lib/interfaces/behaviourObject.interface";
+import { IPositionalConfig } from "./lib/interfaces/positionalConfig.interface";
+import { IRoverController, IRoverInput } from "./lib/interfaces/roverController.interface";
+import { ITerraformState } from "./lib/interfaces/terraformState.interface";
+
+export { VenusRenderer, SetupRenderer, Rover, Terraform, IAudioConfig, IBehaviourObject, IBehaviourObjectChildren, IPositionalConfig, IRoverController, IRoverInput, ITerraformState };
